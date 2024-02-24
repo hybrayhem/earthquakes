e8891feb22f5132bca03b5140266652cd361db3d
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-24 22:17:49 +0300

    concurrent cache and fetch

--- Network/QuakeClient.swift
+++ Network/QuakeClient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class QuakeClient {
+actor QuakeClient { // Making QuakeClient an actor protects the cache from simultaneous access from multiple threads.
     private let quakeCache: NSCache<NSString, CacheEntryObject> = NSCache()
     
     var quakes: [Quake] {
@@ -33,8 +33,37 @@ class QuakeClient {
     }
     
     func quakeLocation(from url: URL) async throws -> QuakeLocation {
-        let data = try await downloader.httpData(from: url)
-        let location = try decoder.decode(QuakeLocation.self, from: data)
-        return location
+        if let cached = quakeCache[url] {
+            switch cached {
+            case .ready(let location):
+                return location
+            case .inProgress(let task):
+                return try await task.value // dont make a new request, if url already cached
+            }
+        }
+        
+        // define location fetching task
+        let task = Task<QuakeLocation, Error> {
+            let data = try await downloader.httpData(from: url)
+            let location = try decoder.decode(QuakeLocation.self, from: data)
+            return location
+        }
+        
+        // add task to cache, like queue
+        quakeCache[url] = .inProgress(task)
+        
+        do {
+            // wait for fetch
+            let location = try await task.value
+            
+            // fetch done, store and return location
+            quakeCache[url] = .ready(location)
+            return location
+            
+        } catch {
+            // remove task from cache
+            quakeCache[url] = nil
+            throw error
+        }
     }
 }
